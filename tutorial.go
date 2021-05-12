e81d9b5268d83f15fc10ee4998bbe42457066833
Alejandro Cervantes <46929938+lifeofal@users.noreply.github.com>
2021-05-12 09:39:14 -0400

    Learning assignment, data types, implicit and explicit declaration and printing to the console.

--- tutorial.go
+++ tutorial.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	var x int = 5
+	z := 10 //implicit
+
+	//z := 10 is equivalent to var z int = 10. GoLang can define the data type automatically.
+	//vars cant be a keyword and cannot start with a number or contain a special char.
+
+	number := 500 //implicitly making 'number' an int variable
+	number = 2    //assigning 'number' to a new value *MUST BE THE SAME DATA TYPE AS VARIABLE CREATION*
+
+	fmt.Println("Hello world", z, x, number)
+
 }
