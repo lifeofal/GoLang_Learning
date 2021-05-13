b821cc5693b3dc4a0c2d0070a5af43a6e0a7f5ad
Alejandro Cervantes <46929938+lifeofal@users.noreply.github.com>
2021-05-13 11:49:15 -0400

    Arithmetic operators and math module complete

---
+++ arithmetic_operators.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 int = 9
+	var num2 int = 10
+	answer := num1 + num2
+	fmt.Printf("%d", answer)
+
+	//B E D M A S: Brackets, Exponents, Division, Multiplication, Addition, Subtraction
+
+	//Mod
+	fmt.Println("\n-----------------------------")
+	var num3 int = 3
+	var num4 int = 14
+	answer2 := num3 % num4
+	fmt.Printf("%d", answer2)
+}
